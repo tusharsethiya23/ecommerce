1507072c95e9234e3456638b67690f146365405f
Tushar Sethiya <tusharsethiya0650@gmail.com>
2026-09-17 18:39:45 +0530

    remove from cart feature implemented

--- server/src/controllers/cart.controller.js
+++ server/src/controllers/cart.controller.js
@@ -1,3 +1,4 @@
+import { resolvePath } from "react-router";
 import cartModel from "../models/cart.model.js";
 import productModel from "../models/product.model.js";
 
@@ -60,5 +61,69 @@ export const addToCart = async(req,res)=>{
     }
 
     return res.status(201).json({message:"product added to the cart successfully"})
+
+}
+
+export const removeFromCart = async(req,res)=>{
+
+    const user = req.user
+
+    const {productId} = req.params
+
+    const {size:productSize, quantity} = req.body
+
+    const product = await productModel.findOne({_id:productId})
+
+    if(!product){
+        return res.status(404).json({message:"product not found"})
+    }
+
+    const cart = await cartModel.findOne({user:req.user.id})
+
+    if(!cart){
+        return res.status(400).json({message:"cart not found"})
+    }
+
+    const isInCart = cart.products.find(p=>p.product.toString() === productId && p.size === productSize)
+
+    if(!isInCart){
+        return res.status(404).json({message:"product not found in cart"})
+    }
+
+   if(quantity>=isInCart.quantity){
+        await cartModel.findOneAndUpdate(
+            {
+                user:req.user.id
+            },
+            {
+                $pull:{
+                    products:{
+                        product:productId,
+                        size:productSize
+                    }
+                }
+            }
+        )
+   }else{
+
+    const newQuantity = isInCart.quantity - quantity
+
+    await cartModel.findOneAndUpdate(
+        {
+            user:req.user.id
+        },
+        {
+           $set:{
+            "products.$[elem].quantity":newQuantity
+           } 
+        },
+        {
+            arrayFilters:[{"elem.product":productId, "elem.size":productSize}]
+        }
+    )
+
+   }
+
+   return res.status(200).json({message:"product removed from cart"})
 
 }
--- server/src/routes/cart.routes.js
+++ server/src/routes/cart.routes.js
@@ -1,12 +1,25 @@
 import express from 'express'
 import { authMiddleware } from '../middleware/auth.middleware.js'
-import { addToCartValidator } from '../validators/cart.validator.js'
-import { addToCart } from '../controllers/cart.controller.js'
+import { addToCartValidator, removeFromCartValidator } from '../validators/cart.validator.js'
+import { addToCart, removeFromCart } from '../controllers/cart.controller.js'
 
 const routes = express.Router()
 
 routes.use(authMiddleware)
 
+/*
+METHOD:post
+add to cart 
+PATH: api/cart
+ */
 routes.post("/add/product/:productId", addToCartValidator, addToCart )
+
+/*
+METHOD:delete
+remove from cart 
+PATH: api/cart
+ */
+
+routes.delete("/remove/product/:productId", removeFromCartValidator ,removeFromCart)
 
 export default routes
